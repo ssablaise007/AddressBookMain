d11f6d113ad78ae5ab4c2f1b33433b1df32bf47a
Your Name <you@example.com>
2021-08-21 09:08:15 +0530

    Added : Creating Contact

---
+++ src/main/java/com/bridgelabz/AddressBookManager.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBookManager {
+
+    List<Contact> addressBookList = new ArrayList();
+    Scanner sc = new Scanner(System.in);
+
+
+}
--- src/main/java/com/bridgelabz/Contact.java
+++ src/main/java/com/bridgelabz/Contact.java
@@ -5,20 +5,17 @@ package com.bridgelabz;
  */
 public class Contact {
 
-    public final String firstname, lastname;
-    public final String address, city, state;
-    public final String zip;
-    public final String phoneNumber;
-    public final String email;
+    public final String firstname, lastname, address, city, state, email;
+    public final int zip, phoneNumber;
 
-    public Contact(String firstname, String lastname, String address, String city, String state, String zip, String phonenumber, String email) {
+    public Contact(String firstname, String lastname, String address, String city, String state, int zip, int phoneNumber, String email) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.address = address;
         this.city = city;
         this.state = state;
         this.zip = zip;
-        this.phoneNumber = phonenumber;
+        this.phoneNumber = phoneNumber;
         this.email = email;
     }
 
@@ -33,10 +30,5 @@ public class Contact {
         System.out.println("Email : " + email);
     }
 
-    public static class UC_1 {
-        public static void main(String args[]) {
-            Contact contact = new Contact("Shoyab", "Blaise", "rangastreet", "ukd", "AndhraPradesh", "515812", "987654322", "ssablaise007@gmail.com");
-            contact.printContactDetails();
-        }
-    }
+
 }
